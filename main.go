353c1ebe22ebd6b4528d0cacab17191eb8ff8e45
rezafauzan <rezafauzan593@gmail.com>
2026-02-12 10:36:13 +0700

    Feat: add interactivity asking temp value and show menu then ask which menu

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+func main() {
+	var temp int
+	for true {
+		var pilihan int
+		fmt.Println("Masukan suhu dalam bentuk celcius")
+		fmt.Scanln(&temp)
+		fmt.Println("1.Konversi ke fahrenheit")
+		fmt.Println("2.Konversi ke kelvin")
+		fmt.Println("3.Konversi ke reamur")
+		fmt.Println("0.Konversi ke reamur")
+		fmt.Scanln(&pilihan)
+	}
 }
