2d793986ab9ead81d51df80996bfac9c80b3ccc9
rezafauzan <rezafauzan593@gmail.com>
2026-02-12 10:39:34 +0700

    Fix: wrong output on pilihan == 1; add exit if pilihan == 0

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	var temp float64
@@ -11,16 +14,20 @@ func main() {
 		fmt.Println("1.Konversi ke fahrenheit")
 		fmt.Println("2.Konversi ke kelvin")
 		fmt.Println("3.Konversi ke reamur")
-		fmt.Println("0.Konversi ke reamur")
+		fmt.Println("0.Keluar Dari Program")
 		fmt.Scanln(&pilihan)
-		if(pilihan == 1){
-			fmt.Printf("Konversi suhu %.1f celcius ke fahrenheit adalah %.1f", temp, ((temp * 9/5) + 32))
+		if pilihan == 1 {
+			fmt.Printf("Konversi suhu %.1f celcius ke fahrenheit adalah %.1f \n", temp, ((temp * 9 / 5) + 32))
 		}
-		if(pilihan == 2){
-			fmt.Printf("Konversi suhu %.1f celcius ke fahrenheit adalah %.1f", temp, (temp + 273.15) )
+		if pilihan == 2 {
+			fmt.Printf("Konversi suhu %.1f celcius ke fahrenheit adalah %.1f \n", temp, (temp + 273.15))
 		}
-		if(pilihan == 1){
-			fmt.Printf("Konversi suhu %.1f celcius ke fahrenheit adalah %.1f", temp, (temp * 4/5))
+		if pilihan == 3 {
+			fmt.Printf("Konversi suhu %.1f celcius ke fahrenheit adalah %.1f \n", temp, (temp * 4 / 5))
+		}
+		if pilihan == 0 {
+			fmt.Println("Pilihan tidak ada !")
+			os.Exit(0)
 		}
 	}
 }
